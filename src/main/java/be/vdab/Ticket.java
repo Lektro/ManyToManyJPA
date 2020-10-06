cb2cc816ebfacb1efd307d3528601435c646c311
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-06 11:45:13 +0200

    ticket made

--- src/main/java/be/vdab/Ticket.java
+++ src/main/java/be/vdab/Ticket.java
@@ -1,10 +1,10 @@
 package be.vdab;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
 public abstract class Ticket {
 
     @Id
@@ -14,4 +14,27 @@ public abstract class Ticket {
     private String departure;
     private String arrival;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
 }
